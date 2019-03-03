5f79df5f26858e4684ec0a0b3bafce3bce08ae06
ibm97531 <e14033213@mail.ncku.edu.tw>
2019-03-03 16:22:26 +0800

    Upload HW_01

--- main.c
+++ main.c
@@ -4,15 +4,17 @@
 int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
-int divided(int a, int b) { return a / b; }
+int divided(int a, int b) { return a / b;}
+
 int main()
 {
 	int a, c;
 	char b;
 	printf("key a Function \nEX: 1 + 1\n");
 	scanf("%i %c %i", &a, &b, &c);
+        int (*classify[])( int, int)={ multiply, plus, NULL,minus, NULL, divided};
 	//IF ELSE IF
-	if (b == '+')
+/*	if (b == '+')
 		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
 	else if (b == '-')
 		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
@@ -20,4 +22,9 @@ int main()
 		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
 	else if (b == '/')
 		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+*/
+        if(b>=0 && b<48)
+        printf("%d %c %d = %d\n", a, b, c, classify[b-42](a,c));
+        return 0;
 }
+
